e0f2deb9bacd1b4498d0ec005c4b823e3dbbf8fe
derekl123 <47576823+derekl123@users.noreply.github.com>
2020-07-09 13:44:53 -0400

    Updates

---
+++ precache-manifest.7498e4a6c8d50beed38371c4559e493f.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80aab9410aa7748c3f49630e5bf14f1",
+    "url": "/gemini/index.html"
+  },
+  {
+    "revision": "c4df45f0581feb9ef408",
+    "url": "/gemini/static/css/2.7204d69a.chunk.css"
+  },
+  {
+    "revision": "1d795d42d2c81f974414",
+    "url": "/gemini/static/css/main.c410e765.chunk.css"
+  },
+  {
+    "revision": "c4df45f0581feb9ef408",
+    "url": "/gemini/static/js/2.2b6ac2e2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gemini/static/js/2.2b6ac2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d795d42d2c81f974414",
+    "url": "/gemini/static/js/main.3bb0465e.chunk.js"
+  },
+  {
+    "revision": "26427fb2cb357cc48562",
+    "url": "/gemini/static/js/runtime-main.778c1df1.js"
+  },
+  {
+    "revision": "0371d636fe20f13f09d2d46240eb862a",
+    "url": "/gemini/static/media/5_fourth.0371d636.png"
+  },
+  {
+    "revision": "66e04af79f9f8ad9d76d37b40cbdbc35",
+    "url": "/gemini/static/media/5_second.66e04af7.png"
+  },
+  {
+    "revision": "f15054288e6eab4177ad767ed2a3cfd2",
+    "url": "/gemini/static/media/5_third.f1505428.png"
+  },
+  {
+    "revision": "4811f9356a06d85baa0eca4dfa6ea38a",
+    "url": "/gemini/static/media/Background.4811f935.jpeg"
+  },
+  {
+    "revision": "c81608a8691900997176354698aab3ca",
+    "url": "/gemini/static/media/image.c81608a8.jpg"
+  },
+  {
+    "revision": "2540249886524b24c3c7a6e62b10e50e",
+    "url": "/gemini/static/media/image2.25402498.jpg"
+  },
+  {
+    "revision": "eeb35fafb2d86ca7c78044f11cc3cebc",
+    "url": "/gemini/static/media/logo.eeb35faf.svg"
+  },
+  {
+    "revision": "0a40085c6d27f28d8b563c26403c3ff1",
+    "url": "/gemini/static/media/productsHero.0a40085c.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/gemini/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/gemini/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/gemini/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/gemini/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9129b69a1f22d11c8a51a29349a2253a",
+    "url": "/gemini/static/media/sneak1.9129b69a.png"
+  },
+  {
+    "revision": "cc8110dbdae22287bf41460df428f1e1",
+    "url": "/gemini/static/media/sneak1_fourth.cc8110db.png"
+  },
+  {
+    "revision": "c03c572c235417b293866200def67cb7",
+    "url": "/gemini/static/media/sneak1_second.c03c572c.png"
+  },
+  {
+    "revision": "524741d7c4008a27ca924b952338ba70",
+    "url": "/gemini/static/media/sneak1_third.524741d7.png"
+  },
+  {
+    "revision": "0680be4d70f0b684f2843702fe9acc5f",
+    "url": "/gemini/static/media/sneak2.0680be4d.png"
+  },
+  {
+    "revision": "2ec36f743fd87d156dfcfbe3777a6997",
+    "url": "/gemini/static/media/sneak2_fourth.2ec36f74.png"
+  },
+  {
+    "revision": "ac52af161f9ead78c17d5ece79e6edf2",
+    "url": "/gemini/static/media/sneak2_second.ac52af16.png"
+  },
+  {
+    "revision": "0be652683ee887aa6bbfef38131e4f41",
+    "url": "/gemini/static/media/sneak2_third.0be65268.png"
+  },
+  {
+    "revision": "2e5fcb7ddf5f5f3630be1fff33ba6bd5",
+    "url": "/gemini/static/media/sneak3.2e5fcb7d.png"
+  },
+  {
+    "revision": "ee368f3db882ea01279826608e838995",
+    "url": "/gemini/static/media/sneak3_fourth.ee368f3d.png"
+  },
+  {
+    "revision": "d281f616b9a0c4d5c8338864ecb379d3",
+    "url": "/gemini/static/media/sneak3_second.d281f616.png"
+  },
+  {
+    "revision": "83343e4c15216cadf632d4e6337fc0ae",
+    "url": "/gemini/static/media/sneak3_third.83343e4c.png"
+  },
+  {
+    "revision": "6a1b297f6a088c326fe22abf65b87762",
+    "url": "/gemini/static/media/sneak4.6a1b297f.png"
+  },
+  {
+    "revision": "da6e30b185f40e02d4fab9a49a315e1c",
+    "url": "/gemini/static/media/sneak4_fourth.da6e30b1.png"
+  },
+  {
+    "revision": "c12d0adb2473b09ba9335d50363dc7ec",
+    "url": "/gemini/static/media/sneak4_second.c12d0adb.png"
+  },
+  {
+    "revision": "55e0b3531a451a78deedd1f6f056f00f",
+    "url": "/gemini/static/media/sneak4_third.55e0b353.png"
+  },
+  {
+    "revision": "f149d58441ef1e566e658169ff7bfd5f",
+    "url": "/gemini/static/media/sneak5.f149d584.png"
+  },
+  {
+    "revision": "20a269985190f1d3e538dab53dfda22d",
+    "url": "/gemini/static/media/sneak6.20a26998.png"
+  },
+  {
+    "revision": "0f90e3a0f3813fc386923886f156e98d",
+    "url": "/gemini/static/media/sneak6_fourth.0f90e3a0.png"
+  },
+  {
+    "revision": "bad6ead1ce87712d2df37f323bb7180c",
+    "url": "/gemini/static/media/sneak6_second.bad6ead1.png"
+  },
+  {
+    "revision": "013e912a7322d58da8d2754826f0a99c",
+    "url": "/gemini/static/media/sneak6_third.013e912a.png"
+  },
+  {
+    "revision": "470815244ca72a7f892970d64cbfca24",
+    "url": "/gemini/static/media/sneak7.47081524.png"
+  },
+  {
+    "revision": "4be9de742c2082f723cd728fbbc0ea00",
+    "url": "/gemini/static/media/sneak7_fourth.4be9de74.png"
+  },
+  {
+    "revision": "2575c3e1ae123b1f03e01d918ba541a2",
+    "url": "/gemini/static/media/sneak7_second.2575c3e1.png"
+  },
+  {
+    "revision": "c0abf76e9b385f8b265cbb678175d929",
+    "url": "/gemini/static/media/sneak7_third.c0abf76e.png"
+  },
+  {
+    "revision": "9a8605221df09c8438fd6af8d95ef721",
+    "url": "/gemini/static/media/sneak8.9a860522.png"
+  },
+  {
+    "revision": "85c7a927d4e1fde03034c2477145dead",
+    "url": "/gemini/static/media/sneak8_fourth.85c7a927.png"
+  },
+  {
+    "revision": "43351d01ef1a953257447f12b16d9225",
+    "url": "/gemini/static/media/sneak8_second.43351d01.png"
+  },
+  {
+    "revision": "b0bb3fe18ba2a06ebcb0d33bf549ed49",
+    "url": "/gemini/static/media/sneak8_third.b0bb3fe1.png"
+  },
+  {
+    "revision": "4bd397f629ba7845b18dbcdce223af90",
+    "url": "/gemini/static/media/sneak9.4bd397f6.png"
+  },
+  {
+    "revision": "78fddab2c371cfa8d69c2ec9a69b74cf",
+    "url": "/gemini/static/media/sneak9_fourth.78fddab2.png"
+  },
+  {
+    "revision": "429d8a87f151cf491fb636b5ccf3aed7",
+    "url": "/gemini/static/media/sneak9_second.429d8a87.png"
+  },
+  {
+    "revision": "04c291d65ec652e6e71dedf68cc8a20e",
+    "url": "/gemini/static/media/sneak9_third.04c291d6.png"
+  }
+]);
